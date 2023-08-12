88dee03b5eb551c6807bb3ed5958f352bab6c0ae
PakDanan <danan_p@yahoo.com>
2023-08-12 19:14:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,28 +9,34 @@ classifier = joblib.load("iris-classifier.joblib")
 # defining the function which will make the prediction using
 # the data which the user inputs
 def prediction(sepal_length, sepal_width, petal_length, petal_width):
-	prediction = classifier.predict(
-		[[sepal_length, sepal_width, petal_length, petal_width]])
-	print(prediction)
+	prediction = classifier.predict([[sepal_length, sepal_width, petal_length, petal_width]])
 	return prediction
 
 # this is the main function in which we define our webpage
 # giving the webpage a title
 st.title("Iris Flower Prediction")
 st.header('IRIS MODEL DEPLOYMENT')
-	
-# the following lines create text boxes in which the user can enter
-# the data required to make the prediction
-sepal_length = st.text_input("Sepal Length", "Type Here")
-sepal_width = st.text_input("Sepal Width", "Type Here")
-petal_length = st.text_input("Petal Length", "Type Here")
-petal_width = st.text_input("Petal Width", "Type Here")
-result =""
+from PIL import Image
+sepal_length = st.sidebar.slider('SEPAL LENGTH', 0.0, 9.0, (5.0))
+sepal_width = st.sidebar.slider('SEPAL WIDTH', 0.0, 4.5, (2.5))
+petal_length = st.sidebar.slider('PETAL LENGTH', 0.0, 8.0, (4.5))
+petal_width = st.sidebar.slider('PETAL WIDTH', 0.0, 3.0, (1.5))
 	
 # the below line ensures that when the button called 'Predict' is clicked,
 # the prediction function defined above is called to make the prediction
 # and store it in the variable result
 if st.button("Predict"):
-	result = prediction(sepal_length, sepal_width, petal_length, petal_width)
-st.success('The output is {}'.format(result))
+	pred = prediction(sepal_length, sepal_width, petal_length, petal_width)
+	if pred == 0:
+		st.success('The Flower is an Iris-setosa')
+		setosa = Image.open('iris_setosa.jpg')
+		st.image(setosa, caption = 'Iris-setosa', width = 300)
+	elif pred == 1:
+		st.success('The Flower is an Iris-versicolor ')
+		versicolor = Image.open('iris_versicolor.jpg')
+		st.image(versicolor, caption = 'Iris-versicolor', width = 300)
+	else:
+		st.success('The Flower is an Iris-virginica ')
+		virginica = Image.open('iris_virginica.jpg')
+		st.image(virginica, caption = 'Iris-virginica', width = 300 )
 	
