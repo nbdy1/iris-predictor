eb36213c19092ad9f40d47968ae88f1175ef1e55
PakDanan <danan_p@yahoo.com>
2023-08-12 19:18:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def prediction(sepal_length, sepal_width, petal_length, petal_width):
 
 # this is the main function in which we define our webpage
 # giving the webpage a title
-st.title("Iris Flower Prediction")
+st.header("Iris Flower Prediction")
 st.sidebar.subheader("Input features")
 sepal_length = st.sidebar.slider('Sepal length', 0.0, 9.0, (5.0))
 sepal_width = st.sidebar.slider('Sepal width', 0.0, 4.5, (2.5))
